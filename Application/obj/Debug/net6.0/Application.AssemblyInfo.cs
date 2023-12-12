a7082f46fede3515aa9f543d2478f8902899c51f
Mohamed Sameh <m.samehmohamed123@gmail.com>
2023-12-12 15:28:47 -0800

    update gitignore

--- Application/obj/Debug/net6.0/Application.AssemblyInfo.cs
+++ Application/obj/Debug/net6.0/Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fd7abfff38997662f2347ac994d5c95df3f48da8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a01b2c19a56a8b5d6ee96743d41a45873bc042ca")]
 [assembly: System.Reflection.AssemblyProductAttribute("Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
